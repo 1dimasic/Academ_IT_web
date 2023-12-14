71fb92ce0db8d0807cf643202c2a9f48cb180e43
Dmitrii <litrox@mail.ru>
2023-12-14 23:40:35 +0700

    js_object task 1.1

--- js_object/script.js
+++ js_object/script.js
@@ -2,45 +2,79 @@
     const countries = [
         {
             name: "England",
-            city: [{name: "London", population: 12000}, {name: "Manchester", population: 550}, {
-                name: "Liverpool", population: 450}]
+            cities: [
+                {
+                    name: "London",
+                    population: 12000
+                },
+                {
+                    name: "Manchester",
+                    population: 550
+                },
+                {
+                    name: "Liverpool",
+                    population: 450
+                }
+            ]
         },
         {
             name: "Russia",
-            city: [{name: "Moscow", population: 13000}, {name: "Kazan", population: 5000}, {
-                name: "Sochi", population: 600}, {name: "Novosibirsk", population: 2500}]
+            cities: [
+                {
+                    name: "Moscow",
+                    population: 13000
+                },
+                {
+                    name: "Kazan",
+                    population: 5000
+                },
+                {
+                    name: "Sochi",
+                    population: 600
+                },
+                {
+                    name: "Novosibirsk",
+                    population: 2500
+                }
+            ]
         },
         {
             name: "Germany",
-            city: [{name: "Berlin", population: 8000}, {name: "Munich", population: 4000}, {
-                name: "Dortmund", population: 1000}]
-        }];
+            cities: [
+                {
+                    name: "Berlin",
+                    population: 8000
+                },
+                {
+                    name: "Munich",
+                    population: 4000
+                },
+                {
+                    name: "Dortmund",
+                    population: 1000
+                }
+            ]
+        }
+    ];
 
-let max_cities_count = 0;
-let max_cities_count_country = [];
-let country_population = {};
-let population;
-
-for (let i = 0; i < countries.length; ++i) {
-    if (countries[i].city.length > max_cities_count) {
-        max_cities_count = countries[i].city.length;
-    }
-}
-
-for (let i = 0; i < countries.length; ++i) {
-    if (countries[i].city.length === max_cities_count) {
-        max_cities_count_country.push(countries[i].name)
-    }
-
-    population = 0;
-
-    for (let j = 0; j < countries[i].city.length; ++j) {
-        population += countries[i].city[j].population;
+    function getMaxCitiesCountCountriesArray() {
+        return countries.filter((e) => e.cities.length === countries.map((e) => e.cities.length).sort(
+            (e1, e2) => e2 - e1)[0]);
     }
 
-    country_population[countries[i].name] = population;
-}
+    function getCountryTotalPopulationObject() {
+        const CountryTotalPopulation = {};
 
-console.log(max_cities_count_country);
-console.log(country_population);
+        countries.forEach((e) => CountryTotalPopulation[e.name] = e.cities.map((city) => city.population).reduce(
+            (e1, e2) => e1 + e2), 0);
+
+        return CountryTotalPopulation;
+    }
+
+    const MaxCitiesCountCountriesArray = getMaxCitiesCountCountriesArray();
+    console.log(MaxCitiesCountCountriesArray);
+
+    const CountryTotalPopulationObject = getCountryTotalPopulationObject();
+    console.log(CountryTotalPopulationObject);
+
 })();
