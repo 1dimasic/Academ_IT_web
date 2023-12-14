95baa021f2b5308e2008f1ed99e83181db126ad0
Dmitrii <litrox@mail.ru>
2023-12-14 21:50:37 +0700

    js_array task 1.1

--- js_array/script.js
+++ js_array/script.js
@@ -1,41 +1,45 @@
 (function () {
-    const array = [2, 4, 5, 7, 1, 46, -25, 0, 4];
-    array.sort(function (e1, e2) {
-        return e2 - e1;
-    });
-    console.log(array);
-})();
-
-(function () {
-    const array = [2, 4, 5, 7, 1, 46, -25, 0, 4];
-    console.log(array.slice(0, 5));
-})();
-
-(function () {
-    const array = [2, 4, 5, 7, 1, 46, -25, 0, 4];
-    console.log(array.slice(-5));
-})();
-
-(function () {
-    const array = [2, 4, 5, 7, 1, 46, -25, 0, 4];
-
-    console.log(array.filter(function (e) {
-        return e % 2 === 0;
-    }).reduce(function (e1, e2) {
-        return e1 + e2;
-    }, 0));
-})();
-
-(function () {
-    const array = [];
-
-    for (let i = 1; i <= 100; ++i) {
-        array.push(i);
+    function sort_array() {
+        array.sort((e1, e2) => e2 - e1);
     }
 
-    console.log(array.filter(function (e) {
-        return e % 2 === 0;
-    }).map(function (e) {
-        return e * e;
-    }, 0));
+    function get_first_five_array() {
+        return array.slice(0, 5);
+    }
+
+    function get_last_five_array() {
+        return array.slice(-5);
+    }
+
+    function get_array_even_numbers_sum() {
+        return array.filter((e) => e % 2 === 0).reduce(
+            (even_number_1, even_number_2) => even_number_1 + even_number_2, 0);
+    }
+
+    function get_squares_even_numbers_array() {
+        const from_one_to_hundred_array = [];
+
+        for (let i = 1; i <= 100; ++i) {
+            from_one_to_hundred_array.push(i);
+        }
+
+        return from_one_to_hundred_array.filter((e) => e % 2 === 0).map((e) => e * e);
+    }
+
+    const array = [2, 4, 5, 7, 1, 46, -25, 0, 4];
+
+    sort_array();
+    console.log(array);
+
+    const first_five_array = get_first_five_array();
+    console.log(first_five_array);
+
+    const last_five_array = get_last_five_array();
+    console.log(last_five_array);
+
+    const array_even_numbers_sum = get_array_even_numbers_sum();
+    console.log(array_even_numbers_sum);
+
+    const squares_even_numbers_array = get_squares_even_numbers_array();
+    console.log(squares_even_numbers_array);
 })();
