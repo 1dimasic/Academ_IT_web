3e5e1bb974c54bfb8fcab2815fe50367e67134b8
Dmitrii <litrox@mail.ru>
2024-01-01 23:12:58 +0700

    jquery_todo_list task 1.1

--- jquery_todo_list/script.js
+++ jquery_todo_list/script.js
@@ -2,29 +2,31 @@ $(function () {
     const form = $(".form");
     const newTask = $(".new_task");
     const todoList = $(".todo_list");
+    const errorMessage = $(".error_message")
 
     form.submit(function (e) {
         e.preventDefault();
 
         let newTaskText = newTask.val().trim();
         newTask.removeClass("invalid");
+        errorMessage.removeClass("invalid");
 
         if (newTaskText.length === 0) {
             newTask.addClass("invalid");
+            errorMessage.addClass("invalid");
             return;
         }
 
         const addedTask = $("<li>").addClass("todo_item");
 
-        function viewContactList() {
+        function updateTodoList() {
             addedTask.html(`<span class="task_text"></span>
-                <button type="button" class="delete_button">Удалить</button>
-                <button type="button" class="edit_button">Редактировать</button>`);
+                <button type="button" class="button delete_button">Удалить</button>
+                <button type="button" class="button edit_button">Редактировать</button>`);
 
             addedTask.find(".task_text").text(newTaskText);
 
             if (addedTask.hasClass("edit_task")) {
-                addedTask.appendTo("edit_task");
                 addedTask.removeClass("edit_task");
             } else {
                 todoList.append(addedTask);
@@ -37,13 +39,13 @@ $(function () {
             addedTask.find(".edit_button").click(function () {
                 addedTask.addClass("edit_task");
                 addedTask.html(`<input type="text" class="edit_task_field">                            
-                    <button type="button" class="cancel_button">Отменить</button>
-                    <button type="button" class="save_button">Сохранить</button>`);
+                    <button type="button" class="button cancel_button">Отменить</button>
+                    <button type="button" class="button save_button">Сохранить</button>`);
 
                 const editTask = addedTask.find(".edit_task_field").val(newTaskText);
 
                 addedTask.find(".cancel_button").click(function () {
-                    viewContactList();
+                    updateTodoList();
                 });
 
                 addedTask.find(".save_button").click(function () {
@@ -52,22 +54,23 @@ $(function () {
 
                     if (editTaskText.length === 0) {
                         editTask.addClass("invalid");
+                        editTask.prop("placeholder", "Введите текст задачи");
                         return;
                     }
 
                     newTaskText = editTaskText;
-                    viewContactList();
+                    updateTodoList();
                 });
 
-                $(document).keypress(function (e) {
+                editTask.keypress(function (e) {
                     if (e.key === "Enter") {
-                        $(".save_button").click();
+                        addedTask.find(".save_button").click();
                     }
                 });
             });
         }
 
-        viewContactList();
-        newTask.text("");
+        updateTodoList();
+        newTask.val("");
     });
 });
