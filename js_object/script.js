03c4e4e0fa22882e340bd26e3d89ab8f95af1b3b
Dmitrii <litrox@mail.ru>
2024-01-06 21:27:17 +0700

    js_object task 1.3

--- js_object/script.js
+++ js_object/script.js
@@ -1,77 +1,81 @@
-const objectsCountriesArray = [
-    {
-        name: "England",
-        cities: [
-            {
-                name: "London",
-                population: 12000
-            },
-            {
-                name: "Manchester",
-                population: 550
-            },
-            {
-                name: "Liverpool",
-                population: 450
-            }
-        ]
-    },
-    {
-        name: "Russia",
-        cities: [
-            {
-                name: "Moscow",
-                population: 13000
-            },
-            {
-                name: "Kazan",
-                population: 5000
-            },
-            {
-                name: "Sochi",
-                population: 600
-            },
-            {
-                name: "Novosibirsk",
-                population: 2500
-            }
-        ]
-    },
-    {
-        name: "Germany",
-        cities: [
-            {
-                name: "Berlin",
-                population: 8000
-            },
-            {
-                name: "Munich",
-                population: 4000
-            },
-            {
-                name: "Dortmund",
-                population: 1000
-            }
-        ]
+(function () {
+    const countries = [
+        {
+            name: "England",
+            cities: [
+                {
+                    name: "London",
+                    population: 12000
+                },
+                {
+                    name: "Manchester",
+                    population: 550
+                },
+                {
+                    name: "Liverpool",
+                    population: 450
+                }
+            ]
+        },
+        {
+            name: "Russia",
+            cities: [
+                {
+                    name: "Moscow",
+                    population: 13000
+                },
+                {
+                    name: "Kazan",
+                    population: 5000
+                },
+                {
+                    name: "Sochi",
+                    population: 600
+                },
+                {
+                    name: "Novosibirsk",
+                    population: 2500
+                }
+            ]
+        },
+        {
+            name: "Germany",
+            cities: [
+                {
+                    name: "Berlin",
+                    population: 8000
+                },
+                {
+                    name: "Munich",
+                    population: 4000
+                },
+                {
+                    name: "Dortmund",
+                    population: 1000
+                }
+            ]
+        }
+    ];
+
+    function getMaxCitiesCountCountriesArray(countries) {
+        const maxCitiesCount = Math.max.apply(null, countries.map(country => country.cities.length));
+        return countries.filter(country => country.cities.length === maxCitiesCount);
     }
-];
 
-function getMaxCitiesCountCountriesArray(countries) {
-    const maxCitiesCount = Math.max.apply(null, countries.map(country => country.cities.length));
-    return countries.filter(country => country.cities.length === maxCitiesCount);
-}
+    function getCountriesTotalPopulationsObject(countries) {
+        const countriesTotalPopulationsObject = {};
 
-function getCountriesTotalPopulationObject(countries) {
-    const countriesTotalPopulation = {};
+        countries.forEach(function (country) {
+            countriesTotalPopulationsObject[country.name] = country.cities
+                .reduce((totalPopulations, city) => totalPopulations + city.population, 0);
+        });
 
-    countries.forEach(country => countriesTotalPopulation[country.name] = country.cities
-        .reduce((totalPopulation, cityPopulation) => totalPopulation + cityPopulation.population, 0));
+        return countriesTotalPopulationsObject;
+    }
 
-    return countriesTotalPopulation;
-}
+    const maxCitiesCountCountriesArray = getMaxCitiesCountCountriesArray(countries);
+    console.log("Countries array with max cities count: ", maxCitiesCountCountriesArray);
 
-const MaxCitiesCountCountriesArray = getMaxCitiesCountCountriesArray(objectsCountriesArray);
-console.log("Countries array with max cities count: ", MaxCitiesCountCountriesArray);
-
-const CountriesTotalPopulationObject = getCountriesTotalPopulationObject(objectsCountriesArray);
-console.log("Object 'Country': Total population: ", CountriesTotalPopulationObject);
+    const countriesTotalPopulationsObject = getCountriesTotalPopulationsObject(countries);
+    console.log("Object 'Country': Total populations: ", countriesTotalPopulationsObject);
+})();
