f6f13fc34d3aad3ef2d02a2855b64c26f36aadd8
Dmitrii <litrox@mail.ru>
2024-01-06 20:41:08 +0700

    js_array task 1.4

--- js_array/script.js
+++ js_array/script.js
@@ -1,52 +1,55 @@
-const testArray = [2, 4, 5, 7, 1, 46, -25, 0, 4];
+(function () {
+    const testArray = [2, 4, 5, 7, 1, 46, -25, 0, 4];
 
-sortDescendingArray(testArray);
-console.log("Descending sort array", testArray)
+    sortDescending(testArray);
+    console.log("Descending sort array", testArray);
 
-const itemsCount = 5;
-const firstFiveItemsArray = getFirstFiveItemsArray(testArray, itemsCount);
-console.log("First five items array: ", firstFiveItemsArray);
+    const itemsCount = 5;
+    const firstSomeItemsArray = getFirstSomeItemsArray(testArray, itemsCount);
+    console.log("First five items array: ", firstSomeItemsArray);
 
-const lastFiveItemsArray = getLastFiveItemsArray(testArray, itemsCount);
-console.log("Last five items array: ", lastFiveItemsArray);
+    const lastSomeItemsArray = getLastSomeItemsArray(testArray, itemsCount);
+    console.log("Last five items array: ", lastSomeItemsArray);
 
-const arrayEvenNumbersSum = getArrayEvenNumbersSum(testArray);
-console.log("Array even numbers sum = ", arrayEvenNumbersSum);
+    const evenNumbersSum = getEvenNumbersSum(testArray);
+    console.log("Array even numbers sum = ", evenNumbersSum);
 
-const arrayEvenNumbersSquares = getArrayEvenNumbersSquares();
-console.log("Even numbers array squares: ", arrayEvenNumbersSquares);
+    const numbersCount = 100;
+    const numbersArray = getNumbersArray(numbersCount);
+    const evenNumbersSquares = getEvenNumbersSquares(numbersArray);
+    console.log("Even numbers array squares: ", evenNumbersSquares);
 
-function sortDescendingArray(array) {
-    array.sort((e1, e2) => e2 - e1);
-}
-
-function getFirstFiveItemsArray(array, itemsCount) {
-    return array.slice(0, itemsCount);
-}
-
-function getLastFiveItemsArray(array, itemsCount) {
-    return array.slice(-itemsCount);
-}
-
-function getArrayEvenNumbersSum(array) {
-    return array
-        .filter(number => number % 2 === 0)
-        .reduce((sum, evenNumber) => sum + evenNumber, 0);
-}
-
-function getNumberArray(maxNumbersCount) {
-    const numberArray = [];
-
-    for (let i = 1; i <= maxNumbersCount; ++i) {
-        numberArray.push(i);
+    function sortDescending(numbersArray) {
+        numbersArray.sort((e1, e2) => e2 - e1);
     }
 
-    return numberArray;
-}
+    function getFirstSomeItemsArray(array, itemsCount) {
+        return array.slice(0, itemsCount);
+    }
 
-function getArrayEvenNumbersSquares() {
-    const maxNumbersCount = 100;
-    return getNumberArray(maxNumbersCount)
-        .filter(number => number % 2 === 0)
-        .map(evenNumber => evenNumber * evenNumber);
-}
+    function getLastSomeItemsArray(array, itemsCount) {
+        return array.slice(-itemsCount);
+    }
+
+    function getEvenNumbersSum(numbersArray) {
+        return numbersArray
+            .filter(number => number % 2 === 0)
+            .reduce((sum, evenNumber) => sum + evenNumber, 0);
+    }
+
+    function getNumbersArray(numbersCount) {
+        const numbersArray = [];
+
+        for (let i = 1; i <= numbersCount; ++i) {
+            numbersArray.push(i);
+        }
+
+        return numbersArray;
+    }
+
+    function getEvenNumbersSquares(numbersArray) {
+        return numbersArray
+            .filter(number => number % 2 === 0)
+            .map(evenNumber => evenNumber * evenNumber);
+    }
+})();
