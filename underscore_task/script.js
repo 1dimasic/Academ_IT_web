add0146650a1e3e90e1ad5286d98464c7072812c
Dmitrii <litrox@mail.ru>
2024-01-20 16:20:17 +0700

    underscore_task 1.1

--- underscore_task/script.js
+++ underscore_task/script.js
@@ -1,15 +1,15 @@
 (function () {
     const people = [
-        {name: 'Bob', age: 25},
-        {name: 'John', age: 20},
-        {name: 'Mike', age: 35},
-        {name: 'Kate', age: 30},
-        {name: 'Pit', age: 50},
-        {name: 'Scott', age: 60},
-        {name: 'Ashley', age: 40},
-        {name: 'Sue', age: 55},
-        {name: 'Tony', age: 19},
-        {name: 'John', age: 21}
+        {name: "Bob", age: 25},
+        {name: "John", age: 20},
+        {name: "Mike", age: 35},
+        {name: "Kate", age: 30},
+        {name: "Pit", age: 50},
+        {name: "Scott", age: 60},
+        {name: "Ashley", age: 40},
+        {name: "Sue", age: 55},
+        {name: "Tony", age: 19},
+        {name: "John", age: 21}
     ];
 
     const averageAge = _.sumBy(people, "age") / people.length;
@@ -22,14 +22,15 @@
 
     console.log("People list from 20 to 30 aged: ", peopleListFromTwentyToThirtyAged);
 
-    const uniqPeopleNamesListFromTwentyToThirtyAged = _.chain(people)
+    const uniquePeopleNamesListFromTwentyToThirtyAged = _.chain(people)
         .filter(p => p.age >= 20 && p.age <= 30)
-        .uniqBy("name")
         .sortBy("age")
         .reverse()
+        .map(p => p.name)
+        .uniq()
         .value();
 
-    console.log("Uniq people names list from 20 to 30 aged: ", uniqPeopleNamesListFromTwentyToThirtyAged);
+    console.log("Unique people names list from 20 to 30 aged: ", uniquePeopleNamesListFromTwentyToThirtyAged);
 
     const namePeopleCountObject = _.countBy(people, "name");
     console.log("\"Name\": people count object - ", namePeopleCountObject);
