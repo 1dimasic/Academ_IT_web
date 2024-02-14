9ed24f3da82cf715131496801327969ba13c0e7a
1dimasic <litrox@mail.ru>
2024-02-14 23:43:11 +0700

    vue_todo_list task 1.2

--- vue_todo_list/js/script.js
+++ vue_todo_list/js/script.js
@@ -1,49 +1,54 @@
 Vue.createApp({
     data() {
         return {
-            todoList: [],
+            todoItems: [],
             id: 0
-        }
+        };
     },
 
     methods: {
         addTodoItem(text) {
+            this.id += 1;
+
             const todoItem = {
-                id: ++this.id,
-                text: text,
-                isEdit: false
-            }
+                id: this.id,
+                text: text
+            };
 
-            this.todoList.push(todoItem);
+            this.todoItems.push(todoItem);
         },
 
-        deleteTodoItem(itemToDelete) {
-            this.todoList = this.todoList.filter(item => item.id !== itemToDelete.id);
-        },
+        deleteTodoItem(itemId) {
+            this.todoItems = this.todoItems.filter(item => item.id !== itemId);
+        }
     },
 
     template: `<h1 class="text-center">Список задач</h1>
-               <component-form @add="addTodoItem"></component-form>
-               <todo-item :item="item" :index="index"
-                          v-for="(item, index) in todoList" :key="item.id"
+               <todo-item-adding-form @add="addTodoItem"></todo-item-adding-form>
+               <todo-item :item="item" 
+                          :index="index"
+                          v-for="(item, index) in todoItems" 
+                          :key="item.id"
                           @remove="deleteTodoItem">
                </todo-item>`
-})
-    .component("ComponentForm", {
+    })
+    .component("TodoItemAddingForm", {
         data() {
             return {
                 newTodoItemText: "",
                 isEmptyNewTodoItemText: false
-            }
+            };
         },
+        
+        emits: ["add"],
 
         computed: {
             validationTodoItemTextClass() {
                 return {
                     "is-valid": this.newTodoItemText.length !== 0,
                     "is-invalid": this.isEmptyNewTodoItemText && this.newTodoItemText.length === 0
-                }
-            },
+                };
+            }
         },
 
         methods: {
@@ -59,101 +64,101 @@ Vue.createApp({
             }
         },
 
-        template: `<form @submit.prevent="submit" class="row" novalidate>
-                    <div class="col-12">
-                      <input type="text" v-model.trim="newTodoItemText"
-                                         class="form-control"
-                                         :class="validationTodoItemTextClass"
-                                         required>
-                      <div class="invalid-feedback text-center">Введите текст задачи</div>
-                    </div>
-                    <div class="col-12 mt-4">
+        template: `
+            <form @submit.prevent="submit" class="row" novalidate>
+                <div class="col-12">
+                  <input type="text" 
+                         v-model.trim="newTodoItemText"
+                         class="form-control"
+                         :class="validationTodoItemTextClass"
+                         required>
+                  <div class="invalid-feedback text-center">Введите текст задачи</div>
+                </div>
+                <div class="col-12 mt-4">
                     <button class="btn btn-primary float-end">Добавить</button>
-                    </div>
-                  </form>`
-
+                </div>
+            </form>`
     })
     .component("TodoItem", {
+        data() {
+            return {
+                todoItemText: this.item.text,
+                isEditMode: false
+            };
+        },
+
+        emits: ["remove"],
+
         props: {
             item: Object,
             index: Number
         },
 
-        data() {
-            return {
-                todoItemText: this.item.text,
-                editMode: this.item.isEdit,
-                savedTodoItemText: ""
-            }
-        },
-
         methods: {
-            remove(item) {
-                this.$emit("remove", item);
+            remove(id) {
+                this.$emit("remove", id);
             },
 
             edit() {
-                this.savedTodoItemText = this.todoItemText;
-                this.editMode = true;
+                this.isEditMode = true;
             },
 
             save() {
-                if (this.todoItemText.length === 0) {
+                if (this.validationTodoItemTextClass["is-invalid"]) {
                     return;
                 }
 
-                this.editMode = false;
+                this.isEditMode = false;
             },
 
             cancel() {
-                if (this.todoItemText.length === 0) {
-                    return;
-                }
-
-                this.todoItemText = this.savedTodoItemText;
-                this.editMode = false;
+                this.item.text = this.todoItemText;
+                this.isEditMode = false;
             }
         },
 
         computed: {
-            editTodoItemValidation() {
+            validationTodoItemTextClass() {
                 return {
-                    "is-valid": this.todoItemText.length !== 0,
-                    "is-invalid": this.todoItemText.length === 0
-                }
+                    "is-valid": this.item.text.length !== 0,
+                    "is-invalid": this.item.text.length === 0
+                };
             }
         },
 
-        template:`<div v-if="!editMode" class="row mt-3">
-                    <div class="col-auto">{{ index + 1 }}</div>
-                    <div class="text-break col">{{ todoItemText }}</div>
-                    <div class="col-auto">
-                      <button @click="remove(item)"
-                              class="btn btn-sm btn-danger float-end">Удалить
-                      </button>
-                      <button @click="edit"
-                              class="btn btn-sm btn-warning float-end me-2">Редактировать
-                      </button>
-                    </div>
-                  </div>
-                  <div v-else class="row mt-3">
-                    <div class="col-auto">{{ index + 1 }}</div>
-                    <div class="col">
-                      <input type="text"
-                             class="form-control"
-                             v-model.trim="todoItemText"
-                             :class="editTodoItemValidation"
-                             @keyup.enter="save">
-                      <div class="invalid-feedback text-center">Введите текст задачи</div>
-                    </div>
-                    <div class="col-auto">
-                      <button @click="cancel"
-                              class="btn btn-sm btn-secondary float-end">Отменить
-                      </button>
-                      <button @click="save"
-                              class="btn btn-sm btn-success float-end me-2">Сохранить
-                      </button>
-                    </div>
-                  </div>`
+        template:`
+            <div v-if="!isEditMode" class="row mt-3">
+                <div class="col-auto">{{ index + 1 }}</div>
+                <div class="text-break col">{{ item.text }}</div>
+                <div class="col-auto">
+                    <button @click="remove(item.id)"
+                            class="btn btn-sm btn-danger float-end">
+                        Удалить
+                    </button>
+                    <button @click="edit"
+                            class="btn btn-sm btn-warning float-end me-2">
+                        Редактировать
+                    </button>
+                </div>
+            </div>
+            <div v-else class="row mt-3">
+                <div class="col-auto">{{ index + 1 }}</div>
+                <div class="col">
+                    <input type="text"
+                           class="form-control"
+                           v-model.trim="item.text"
+                           :class="validationTodoItemTextClass"
+                           @keyup.enter="save">
+                    <div class="invalid-feedback text-center">Введите текст задачи</div>
+                </div>
+                <div class="col-auto">
+                    <button @click="cancel"
+                            class="btn btn-sm btn-secondary float-end">Отменить
+                    </button>
+                    <button @click="save"
+                            class="btn btn-sm btn-success float-end me-2">Сохранить
+                    </button>
+                </div>
+            </div>`
     })
     .mount("#app");
